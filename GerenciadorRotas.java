4671bee3e15a7880eb7ba84eade350adb7afc87d
Erick-Conde <erick.conde@edu.pucrs.br>
2022-09-29 22:09:01 -0300

    teste

--- GerenciadorRotas.java
+++ GerenciadorRotas.java
@@ -8,4 +8,19 @@ public class GerenciadorRotas{
     {
         Collections.sort(rotas);
     }
+
+    public ArrayList <Rota> listarTodas() {return rotas;}
+
+    public ArrayList <Rota> buscarPorOrigem (Aeroporto orig) 
+    {
+        ArrayList <Rota> aux = new ArrayList<>();
+        for (Rota r: rotas) 
+        {
+            if (r.getOrigem().equals(orig)) 
+            {
+                aux.add(r);
+            }
+        }
+        return aux;
+    }
 }
